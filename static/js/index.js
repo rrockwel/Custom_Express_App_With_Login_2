77bd236a67a94b48e51f535c5acd42a39a62a7ce
rrockwel <richardtylerrockwell@gmail.com>
2019-08-11 13:55:37 -0400

    Added Non-Functional Contact&Feedback Forms

--- static/js/index.js
+++ static/js/index.js
@@ -4,7 +4,7 @@ $(document).ready(()=>{
 		document.getElementById('overlay').style.display = 'flex';
 		// Collapse Dropdown Menu
 		document.getElementById('navDropdown').classList.remove('show');
-		
+		// If Login Tab Is Currently Active in overlay, change to Register being Active and shown
 		if(document.getElementById('loginNav').classList.contains('active')){
 			console.log('need to change');
 			document.getElementById('registerNav').classList.add('active');
@@ -17,12 +17,13 @@ $(document).ready(()=>{
 			document.getElementById('registerForm').classList.add('show');
 		}
 	});
+
 	$('#loginNavLink').on('click', ()=>{
 		// Display Overlay
 		document.getElementById('overlay').style.display='flex';
 		// Close Dropdown Menu
 		document.getElementById('navDropdown').classList.remove('show');
-
+		// If register tab is currently active, change to login being active and shown
 		if(document.getElementById('registerNav').classList.contains('active')){
 			console.log('need to change');
 			document.getElementById('registerNav').classList.remove('active');
@@ -36,15 +37,37 @@ $(document).ready(()=>{
 		}
 	});
 
+	// Close overlay when click on x in corner
 	$('#closeForm').on('click', ()=>{
 		document.getElementById('overlay').style.display = 'none';
 	});
-
+	// Bring up logout verification overlay form when logout nav is clicked
 	$('#logoutNavLink').on('click', ()=>{
 		document.getElementById('logoutOverlay').style.display = 'flex';
 	});
-
+	// Close logout overlay if logout 'no' is clicked
 	$('#logoutCancel').on('click', ()=>{
 		document.getElementById('logoutOverlay').style.display = 'none';
 	});
+	// Display feedback overlay when feedback nav is clicked
+	$('#feedbackNav').on('click', ()=>{
+		console.log('display feedback form');
+		document.getElementById('feedback').style.display = 'flex';
+	});
+	// Close feedback overlay when x is clicked
+	$('#closeFeedbackForm').on('click', ()=>{
+		console.log('closing feedbackForm');
+		document.getElementById('feedback').style.display = 'none';
+	});
+
+	
+	// Display Contact overlay when contact nav is clicked
+	$('#contactNav').on('click', ()=>{
+		document.getElementById('contact').style.display = 'flex';
+	});
+	// Close contact overlay when x is clicked
+	$('#closeContactForm').on('click', ()=>{
+		document.getElementById('contact').style.display = 'none';
+	});
+
 });
